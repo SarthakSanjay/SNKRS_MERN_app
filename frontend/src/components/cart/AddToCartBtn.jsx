d705b53f79a6502dc7f2a5c414d81bc25a943f24
Sarthak Sanjay <sarthaksanjaycoll21@gmail.com>
2023-10-06 23:14:31 +0530

    added addToCart functionality

--- frontend/src/components/cart/AddToCartBtn.jsx
+++ frontend/src/components/cart/AddToCartBtn.jsx
@@ -1,8 +1,14 @@
 import { LiaShoppingBagSolid } from "react-icons/lia"
+import axios from 'axios'
 
-const AddToCartBtn = () => {
-  return (
-    <button className='w-52 h-10 bg-white hover:bg-black hover:text-white rounded-full flex justify-center items-center text-black'>Add to Cart
+const AddToCartBtn = ({addToCart}) => {
+  const handleClick = () =>{
+    alert('add to cart')
+    
+    addToCart()
+  }
+   return (
+    <button onClick={handleClick} className='w-52 h-10 bg-white hover:bg-black hover:text-white rounded-full flex justify-center items-center text-black'>Add to Cart
     <LiaShoppingBagSolid className="text-2xl" />
     </button>
   )
