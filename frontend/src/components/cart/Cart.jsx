5fe8746960f66ff2dfd4ca112f5f3c57a389f99f
Sarthak Sanjay <sarthaksanjaycoll21@gmail.com>
2023-10-06 23:17:29 +0530

    added cart component to get rendered

--- frontend/src/components/cart/Cart.jsx
+++ frontend/src/components/cart/Cart.jsx
@@ -1,7 +1,37 @@
+import React, { useState, useEffect } from "react";
+import axios from 'axios';
+import CartItems from "./CartItems";
+
 const Cart = () => {
+  const [cart, setCart] = useState([]);
+  const [total , setTotal] = useState(0)
+  
+
+  useEffect(() => {
+    // Fetch cart data when component mounts
+    axios.get('http://localhost:3000/cart')
+      .then((response) => {
+        console.log(response.data.shoe);
+        setCart(response.data.shoe);
+        // return response.data.shoe
+      })
+      // .then(res => console.log(res))
+      .catch((error) => {
+        console.error('Error fetching cart data:', error);
+      });
+  }, []);  // Empty dependency array to ensure this runs once when component mounts
+
   return (
-    <div className='bg-slate-900 min-h-screen text-white w-screen'>Cart</div>
-  )
+    <div className='bg-slate-900 min-h-screen text-white w-screen flex flex-col items-center p-10'>
+      {cart.map((cartItem) => {
+        {/* setTotal( cartItem.shoeId.price) */}
+        
+
+        return <CartItems key={cartItem._id} cartItem={cartItem.shoeId} id={cartItem._id} />
+      })}
+      {total}
+    </div>
+  );
 }
 
-export default Cart
+export default Cart;
