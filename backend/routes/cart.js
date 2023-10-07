81c4b029cef57e3cd4218948bdc7194e664165ee
Sarthak Sanjay <sarthaksanjaycoll21@gmail.com>
2023-10-07 23:53:22 +0530

    improve cart routes

--- backend/routes/cart.js
+++ backend/routes/cart.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const router = express.Router()
 const {getCartItems , deleteAllCartItems , deleteCartItem , addToCart} = require('../controllers/cartController')
-router.route('/').get(getCartItems).delete(deleteAllCartItems).post(addToCart)
+router.route('/').get(getCartItems)
+router.route('/add').post(addToCart)
+router.route('/deleteAll').delete(deleteAllCartItems)
 router.route('/:id').delete(deleteCartItem)
 
 module.exports = router
