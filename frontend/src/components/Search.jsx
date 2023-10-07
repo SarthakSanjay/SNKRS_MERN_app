813f5244098a33b9c10baa57ccff1df8f6a77c32
Sarthak Sanjay <sarthaksanjaycoll21@gmail.com>
2023-10-07 23:31:11 +0530

    fix style

--- frontend/src/components/Search.jsx
+++ frontend/src/components/Search.jsx
@@ -2,8 +2,8 @@ import {CiSearch} from "react-icons/ci"
 const Search = () => {
   return (
     <>
-    <div className='bg-gray-200 border-gray-500 flex h-10 rounded-full justify-start items-center border-2 pl-2 pr-2'>
-        <CiSearch className='ml-3 text-2xl'/>
+    <div className='bg-gray-200 border-gray-500 flex h-10 rounded-[5px] justify-start items-center border-2 pl-2 pr-2'>
+        <CiSearch className='mr-3 text-2xl text-gray-500'/>
         <input className='bg-gray-200 w-36 outline-none cursor-pointer' placeholder='search' />
 
     </div>
