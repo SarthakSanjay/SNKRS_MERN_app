b464cc27c154fab31ba0c5a32a15f6ed0689459c
Sarthak Sanjay <sarthaksanjaycoll21@gmail.com>
2023-10-08 23:58:01 +0530

    added wishlistSlice

--- frontend/src/store/wishlistSlice.js
+++ frontend/src/store/wishlistSlice.js
@@ -1,5 +1,43 @@
-import {createSlice} from '@reduxjs/toolkit'
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
 
 const initialState = {
-    
-}
+  shoe: [],
+  loading: false,
+  error: null,
+  total: 0
+};
+
+const fetchWishlist = createAsyncThunk('wishlistSlice/fetchWishlist', async () => {
+  try {
+    const response = await axios.get('http://localhost:3000/wishlist');
+    return response.data.shoe; // Return the 'shoe' data from the response
+  } catch (error) {
+    throw new Error('Error fetching wishlist: ' + error.message);
+  }
+});
+
+const wishlistSlice = createSlice({
+  name: 'wishlist',
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchWishlist.pending, (state) => {
+        state.loading = true;
+      })
+      .addCase(fetchWishlist.fulfilled, (state, action) => {
+        state.shoe = action.payload;
+        state.loading = false;
+        state.error = null;
+        state.total = action.payload.length
+      })
+      .addCase(fetchWishlist.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.error.message;
+      });
+  },
+});
+
+export { fetchWishlist };
+export default wishlistSlice.reducer;
