61e36006a48aa3db9d1c8bad11c7e8f91fcda55e
Sarthak Sanjay <sarthaksanjaycoll21@gmail.com>
2023-10-08 23:55:47 +0530

    redux implementation

--- frontend/src/components/wishlist/WishlistBtn.jsx
+++ frontend/src/components/wishlist/WishlistBtn.jsx
@@ -1,12 +1,16 @@
 import { useState } from 'react'
 import {LiaHeart, LiaHeartSolid} from 'react-icons/lia'
+import {fetchWishlist} from '../../store/wishlistSlice'
+import { useDispatch } from 'react-redux'
 const WishlistBtn = ({handleWishlist}) => {
   const [clicked ,setClicked] = useState(false)
+  const dispatch = useDispatch()
     // console.log(btnActive)
     const handleClick = () =>{
       setClicked(prev => !prev)
       handleWishlist()
       alert("added to wishlist")
+      dispatch(fetchWishlist())
     }
   return (
     <button onClick={handleClick }  className='w-52 h-10 bg-black hover:bg-white hover:text-black rounded-full flex justify-center items-center text-white'>
