a60481077bc80d00df4172d90ebf4ab15f69ca13
Sarthak Sanjay <sarthaksanjaycoll21@gmail.com>
2023-10-08 23:56:40 +0530

    added provider and store

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,58 +1,58 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { RouterProvider, createBrowserRouter } from 'react-router-dom'
-import Shoes from './components/Shoes.jsx'
-import ShoesDetails from './components/ShoesDetails.jsx'
-import Wishlist from './components/wishlist/Wishlist.jsx'
-import Men from './components/navItems/Men.jsx'
-import Women from './components/navItems/Women.jsx'
-import  Unisex from './components/navItems/Unisex.jsx'
-import Cart from './components/cart/Cart.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Shoes from "./components/Shoes.jsx";
+import ShoesDetails from "./components/ShoesDetails.jsx";
+import Wishlist from "./components/wishlist/Wishlist.jsx";
+import Men from "./components/navItems/Men.jsx";
+import Women from "./components/navItems/Women.jsx";
+import Unisex from "./components/navItems/Unisex.jsx";
+import Cart from "./components/cart/Cart.jsx";
+import { Provider } from "react-redux";
+import store from "./store/store.js";
 
 const router = createBrowserRouter([
   {
-    path : '/',
+    path: "/",
     element: <App />,
     children: [
       {
         path: "",
-        element: <Shoes />
+        element: <Shoes />,
       },
       {
         path: "category/men",
-        element : <Men />
+        element: <Men />,
       },
       {
         path: "category/women",
-        element : <Women />
+        element: <Women />,
       },
       {
         path: "category/unisex",
-        element : <Unisex />
+        element: <Unisex />,
       },
       {
-        path:"shoe/:id",
-        element : <ShoesDetails />
+        path: "shoe/:id",
+        element: <ShoesDetails />,
       },
       {
-        path:'wishlist',
-        element : <Wishlist />
+        path: "wishlist",
+        element: <Wishlist />,
       },
       {
-        path:'cart',
-        element : <Cart />
+        path: "cart",
+        element: <Cart />,
       },
-
-    ]
-    
-    
-  }
-])
-ReactDOM.createRoot(document.getElementById('root')).render(
+    ],
+  },
+]);
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    {/* <App /> */}
-    <RouterProvider router={router} />
-  </React.StrictMode>,
-)
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
+  </React.StrictMode>
+);
