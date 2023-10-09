c9b47fe213e88199e41d19f4a1b3904befb65ec6
Sarthak Sanjay <sarthaksanjaycoll21@gmail.com>
2023-10-09 23:46:15 +0530

    code fix

--- frontend/src/components/cart/AddToCartBtn.jsx
+++ frontend/src/components/cart/AddToCartBtn.jsx
@@ -1,10 +1,12 @@
 import { LiaShoppingBagSolid } from "react-icons/lia"
-import axios from 'axios'
-
+import { fetchCart } from "../../store/cartSlice"
+import { useDispatch } from "react-redux"
 const AddToCartBtn = ({addToCart}) => {
+  const dispatch = useDispatch()
   const handleClick = () =>{
-    alert('add to cart')
     addToCart()
+    alert('add to cart')
+    dispatch(fetchCart())
   }
    return (
     <button onClick={handleClick} className='w-52 h-10 bg-white hover:bg-black hover:text-white rounded-full flex justify-center items-center text-black'>Add to Cart
