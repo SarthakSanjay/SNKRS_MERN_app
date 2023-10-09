04df00049553a9b5d39188daef4c805d44602be6
Sarthak Sanjay <sarthaksanjaycoll21@gmail.com>
2023-10-09 23:48:26 +0530

    code fix

--- frontend/src/components/wishlist/WishlistBtn.jsx
+++ frontend/src/components/wishlist/WishlistBtn.jsx
@@ -1,22 +1,63 @@
-import { useState } from 'react'
-import {LiaHeart, LiaHeartSolid} from 'react-icons/lia'
-import {fetchWishlist} from '../../store/wishlistSlice'
-import { useDispatch } from 'react-redux'
-const WishlistBtn = ({handleWishlist}) => {
-  const [clicked ,setClicked] = useState(false)
-  const dispatch = useDispatch()
-    // console.log(btnActive)
-    const handleClick = () =>{
-      setClicked(prev => !prev)
-      handleWishlist()
-      alert("added to wishlist")
-      dispatch(fetchWishlist())
-    }
+import { useEffect, useState } from "react";
+import { LiaHeart, LiaHeartSolid } from "react-icons/lia";
+import { fetchWishlist } from "../../store/wishlistSlice";
+import { useDispatch } from "react-redux";
+import axios from "axios";
+
+const WishlistBtn = ({  text , id}) => {
+  const [clicked, setClicked] = useState(false);
+  const dispatch = useDispatch();
+  
+   const getWishlist = () => {
+    axios.post("http://localhost:3000/wishlist/add", {
+      _id: id,
+    });
+  };
+  const handleClick = () => {
+    getWishlist()
+    alert("added to wishlist");
+    dispatch(fetchWishlist());
+
+  };
+
+  useEffect(()=>{
+    axios.get('http://localhost:3000/wishlist')
+    .then((res) => {
+      if(res.data.shoe._id === id){
+        setClicked(true)
+      }else{
+        setClicked(false)
+      }
+    })
+  },[])
+
+
+  if (!text) {
+    return (
+      <button
+        onClick={handleClick}
+        className="w-10 h-10 bg-black hover:bg-white hover:text-black rounded-full flex justify-center items-center text-white"
+      >
+        {!clicked ? (
+          <LiaHeart className="text-2xl " />
+        ) : (
+          <LiaHeartSolid className=" text-2xl text-pink-400" />
+        )}
+      </button>
+    );
+  }
   return (
-    <button onClick={handleClick }  className='w-52 h-10 bg-black hover:bg-white hover:text-black rounded-full flex justify-center items-center text-white'>
-        Favourite 
-        {!clicked ? <LiaHeart className="ml-2 text-2xl "/> : <LiaHeartSolid className="ml-2 text-2xl text-pink-400" />}
+    <button
+      onClick={handleClick}
+      className="w-52 h-10 bg-black hover:bg-white hover:text-black rounded-full flex justify-center items-center text-white"
+    >
+      {text ? "Favourite" : ""}
+      {!clicked ? (
+        <LiaHeart className="ml-2 text-2xl " />
+      ) : (
+        <LiaHeartSolid className="ml-2 text-2xl text-pink-400" />
+      )}
     </button>
-  )
-}
-export default WishlistBtn
+  );
+};
+export default WishlistBtn;
