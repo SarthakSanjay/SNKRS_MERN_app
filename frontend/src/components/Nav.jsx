c093908da7a3b192102d72f0f7eff040949271c2
Sarthak Sanjay <sarthaksanjaycoll21@gmail.com>
2023-10-09 23:43:55 +0530

    improvements

--- frontend/src/components/Nav.jsx
+++ frontend/src/components/Nav.jsx
@@ -1,27 +1,19 @@
 import Logo from "./navItems/Logo";
 import Search from "./Search";
-import { Link } from "react-router-dom";
-import {  useState } from "react";
 import WishListIcon from "./wishlist/WishListIcon";
 import CartIcon from "./cart/CartIcon";
 import NavLinks from "./navItems/NavLinks";
 
-
 const Nav = () => {
- 
- const [count ,setCount] = useState(0)
-  
   return (
     <div className="flex justify-evenly bg-zinc-800 h-14 items-center w-screen sticky top-0">
-        <Logo className="w-1/3 " />
-        <NavLinks />
+      <Logo className="w-1/3 " />
+      <NavLinks />
       <div className="flex items-center w-1/3 justify-around">
         <Search />
-        <WishListIcon  />
+        <WishListIcon />
         <CartIcon />
-        
       </div>
-      <button onClick={() => setCount(count + 1)}>Refresh</button>
     </div>
   );
 };
