c8974e8c79fd50dd3c087bd16d3766def9555eb5
Sarthak Sanjay <sarthaksanjaycoll21@gmail.com>
2023-10-09 23:45:24 +0530

    code fix

--- frontend/src/components/Shoes.jsx
+++ frontend/src/components/Shoes.jsx
@@ -1,39 +1,46 @@
-import { useEffect } from "react"
-import ShoeCard from "./ShoeCard"
-import {useDispatch , useSelector} from 'react-redux'
-import { fetchShoes } from "../store/shoeSlice"
+import { useEffect } from "react";
+import ShoeCard from "./ShoeCard";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchShoes } from "../store/shoeSlice";
 const Shoes = () => {
-    const dispatch = useDispatch()
-    const {shoes , loading , error} = useSelector(state => state.shoe)
+  const dispatch = useDispatch();
+  const { shoes, loading, error } = useSelector((state) => state.shoe);
 
-    useEffect(()=>{
-        dispatch(fetchShoes())
-    },[dispatch])
+  useEffect(() => {
+    dispatch(fetchShoes());
+  }, [dispatch]);
 
-    if (loading) {
-        return <div className='bg-black h-screen w-screen flex justify-center items-center'>
-          <h1 className='text-white text-[40px]'>Loading ...</h1>
-        </div>;
-      }
-      if (error) {
-        return <div className='bg-black h-screen w-screen flex justify-center items-center'>
-          <h1 className='text-white text-[40px]'>Something went wrong!!!</h1>
-        </div>;
-      }
+  if (loading) {
+    return (
+      <div className="bg-black h-screen w-screen flex justify-center items-center">
+        <h1 className="text-white text-[40px]">Loading ...</h1>
+      </div>
+    );
+  }
+  if (error) {
+    return (
+      <div className="bg-black h-screen w-screen flex justify-center items-center">
+        <h1 className="text-white text-[40px]">Something went wrong!!!</h1>
+      </div>
+    );
+  }
 
   return (
     <div className="w-screen p-10 flex flex-wrap justify-center">
-    <img src="https://img.freepik.com/free-vector/modern-black-friday-sale-banner-template-with-3d-background-red-splash_1361-1877.jpg?w=1060&t=st=1696705876~exp=1696706476~hmac=d8bade4b3fdb88be9a895225cedd4f4c126b081199f549f510bb9668b4ff352c" className="h-[60vh] w-[90%]  " />
-    {
-        shoes.map((shoe) => (
-            <div key={shoe._id}>
-                <ShoeCard shoe={shoe} id={shoe._id} />
-          
-            </div>
-        ))
-    }
+      <img
+        src="https://img.freepik.com/free-vector/modern-black-friday-sale-banner-template-with-3d-background-red-splash_1361-1877.jpg?w=1060&t=st=1696705876~exp=1696706476~hmac=d8bade4b3fdb88be9a895225cedd4f4c126b081199f549f510bb9668b4ff352c"
+        className="h-[60vh] w-[90%]  "
+      />
+      {shoes.map((shoe) => {
+        {/* console.log("mofo",shoe); */}
+        return (
+          <div key={shoe._id}>
+            <ShoeCard shoe={shoe} id={shoe._id} />
+          </div>
+        );
+      })}
     </div>
-  )
-}
+  );
+};
 
-export default Shoes
+export default Shoes;
