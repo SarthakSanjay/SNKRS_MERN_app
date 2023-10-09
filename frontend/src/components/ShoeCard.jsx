55d6021bf2e5a847b3d6e8d4bb4cbf0825e40ee9
Sarthak Sanjay <sarthaksanjaycoll21@gmail.com>
2023-10-09 23:44:53 +0530

    code fix

--- frontend/src/components/ShoeCard.jsx
+++ frontend/src/components/ShoeCard.jsx
@@ -1,24 +1,25 @@
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
+import WishlistBtn from "./wishlist/WishlistBtn";
 
-const ShoeCard = ({shoe,id}) => {
-    const {productName,price, image} = shoe
-    const handleClick = () =>{
-        
-    }
+const ShoeCard = ({ shoe, id }) => {
+  if (!shoe) {
+    return null; 
+  }
+  const { productName, price, image } = shoe;
   return (
     <>
-    <Link to={`/shoe/${id}`}>
-    <div className=" h-max w-52 m-10 cursor-pointer text-white"
-    onClick={handleClick}
-    >
-        <img src={image[1]} className="w-full object-cover h-40" />
-        <h3 className="ml-2 p-1">{productName}</h3>
-        <h3 className="ml-2 p-1">Price: ${price}</h3>
-
-    </div>
-    </Link>
+      <div className=" h-max w-52 m-10 cursor-pointer text-white">
+        <Link to={`/shoe/${id}`}>
+          <img src={image[1]} className="w-full object-cover h-40" />
+          <h3 className="ml-2 p-1">{productName}</h3>
+          <h3 className="ml-2 p-1">Price: ${price}</h3>
+        </Link>
+        <div className=" relative left-40 bottom-12 ">
+          <WishlistBtn text={false} id={id} />
+        </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default ShoeCard
+export default ShoeCard;
