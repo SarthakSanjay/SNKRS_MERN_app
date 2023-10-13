1a2a1f0fcf048a0eb67e58311b1d61903c7296b6
Sarthak Sanjay <sarthaksanjaycoll21@gmail.com>
2023-10-13 23:46:02 +0530

    some changes to the code

--- frontend/src/components/cart/CartItems.jsx
+++ frontend/src/components/cart/CartItems.jsx
@@ -1,60 +1,80 @@
-import React, { useEffect, useState } from 'react'
-import {AiOutlineArrowUp ,AiOutlineArrowDown} from 'react-icons/ai'
-import axios from 'axios'
-import { fetchCart } from '../../store/cartSlice'
-import { useDispatch , useSelector } from 'react-redux'
-import { calculateTotalAmount } from '../../store/cartSlice'
+import React, { useEffect, useState } from "react";
+import { AiOutlineArrowUp, AiOutlineArrowDown } from "react-icons/ai";
+import axios from "axios";
+import { fetchCart } from "../../store/cartSlice";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  calculateTotalAmount,
+  increase,
+  decrease,
+} from "../../store/cartSlice";
 
-const CartItems = ({cartItem,id}) => {
-    const dispatch = useDispatch()
-    const {totalAmount} = useSelector(state => state.cart)
-    const [quantity , setQuantity] = useState(1)
-    const [amount , setAmount] = useState(cartItem.price)
-    useEffect(()=>{
-        setAmount(cartItem.price * quantity)
+const CartItems = ({ cartItem, id }) => {
+  const dispatch = useDispatch();
+  const {quantity} = useSelector(state => state.cart)
+  // const {totalAmount} = useSelector(state => state.cart)
+  // const [quantity , setQuantity] = useState(1)
+  // const [amount , setAmount] = useState(cartItem.price)
+  useEffect(() => {
+    // setAmount(cartItem.price * quantity);
 
-        dispatch(calculateTotalAmount(quantity))
-        // console.log(totalAmount);
-        
+    dispatch(calculateTotalAmount(quantity));
+    // console.log(totalAmount);
+  }, [quantity, cartItem.price]);
 
-    },[quantity , cartItem.price])
+  // console.log('cartitems', cartItem)
+  const delShoeFromCart = () => {
+    axios
+      .delete(`http://localhost:3000/cart/${id}`)
+      .then(console.log("item deleted"))
+      .then(dispatch(fetchCart()));
+    alert("item deleted");
+  };
+  // const increment = () =>{
+  //     setQuantity(quantity + 1)
 
-   
-    // console.log('cartitems', cartItem)
-    const delShoeFromCart = () =>{
-        axios.delete(`http://localhost:3000/cart/${id}`)
-        .then(console.log("item deleted"))
-        .then(dispatch(fetchCart()))
-        alert('item deleted')
-    }
-    const increment = () =>{
-        setQuantity(quantity + 1)
-        
-    }
-    const decrement = () =>{
-        if(quantity === 1) return
-        setQuantity(quantity - 1)
-    }
+  // }
+  // const decrement = () =>{
+  //     if(quantity === 1) return
+  //     setQuantity(quantity - 1)
+  // }
   return (
-    <div className='bg-gray-800 h-[100px] w-[500px] flex justify-between items-center m-5 '>
-        <img src={cartItem.image[0]} className='h-full w-1/5 object-center ' />
-        <div className='flex flex-col justify-around h-full '>
+    <div className="bg-gray-800 h-[100px] w-[500px] flex justify-between items-center m-5 ">
+      <img src={cartItem.image[0]} className="h-full w-1/5 object-center " />
+      <div className="flex flex-col justify-around h-full ">
         <h2>{cartItem.productName}</h2>
-        <div className='bg-red-400 w-[120px] rounded-[5px] flex justify-between p-2'>
-            <button onClick={increment} className='bg-gray-500 border-[1px] border-black rounded-[2px]'>
-                <AiOutlineArrowUp />
-            </button>
-            Qty : {quantity}
-            <button onClick={decrement} className='bg-gray-500 border-[1px] border-black rounded-[2px]'>
-                <AiOutlineArrowDown />
-            </button>
-        </div>
-        </div>
-            <h1>${amount}</h1>
-            
-            <button onClick={delShoeFromCart} className='bg-red-900 h-[40px] w-20  p-2 mr-2 rounded-[2px] hover:bg-red-500'>remove</button>
-    </div>
-  )
-}
+        <div className="bg-red-400 w-[120px] rounded-[5px] flex justify-between p-2">
+          <button
+            onClick={() => {
+                dispatch(increase(id))
+                dispatch(fetchCart())
+            }}
+            className="bg-gray-500 border-[1px] border-black rounded-[2px]"
+          >
+            <AiOutlineArrowUp />
+          </button>
+          Qty : {quantity}
+          <button
+            onClick={()=>{
 
-export default CartItems
+                dispatch(decrease(id))
+            }}
+            className="bg-gray-500 border-[1px] border-black rounded-[2px]"
+          >
+            <AiOutlineArrowDown />
+          </button>
+        </div>
+      </div>
+      {/* <h1>${amount}</h1> */}
+
+      <button
+        onClick={delShoeFromCart}
+        className="bg-red-900 h-[40px] w-20  p-2 mr-2 rounded-[2px] hover:bg-red-500"
+      >
+        remove
+      </button>
+    </div>
+  );
+};
+
+export default CartItems;
--- frontend/src/store/cartSlice.js
+++ frontend/src/store/cartSlice.js
@@ -1,21 +1,20 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
-import { stat } from 'fs';
-import cart from '../../../backend/models/cart';
 
 const initialState = {
   cart: [],
   loading: false,
   error: null,
   total: 0,
-  totalAmount: 0
+  totalAmount: 0,
+  quantity: 1
 };
 const url = 'http://localhost:3000/cart'
 
 const fetchCart = createAsyncThunk('cartSlice/fetchCart', async () => {
   try {
     const response = await axios.get(url);
-    console.log(response.data.shoe)
+    // console.log(response.data.shoe)
     return response.data.shoe; // Return the 'shoe' data from the response
   } catch (error) {
     throw new Error('Error fetching wishlist: ' + error.message);
@@ -27,8 +26,9 @@ const cartSlice = createSlice({
   initialState,
   reducers: {
     increase: (state , {payload}) =>{
-      const cartItem = state.cart.find((item) => item._id === payload.id)
-      cartItem.quantity += cartItem.quantity + 1
+      // const cartItem = state.cart.find((item) => item._id === payload.id)
+      // cartItem.quantity += cartItem.quantity + 1
+      console.log()
     },
     decrease: (state , {payload}) =>{
       const cartItem = state.cart.find((item) => item._id === payload.id)
@@ -55,14 +55,18 @@ const cartSlice = createSlice({
         state.loading = false;
         state.error = null;
         state.total = action.payload.length
+        
+        state.quantity = action.payload[0].quantity
+        // console.log("payload",action.payload[0].quantity)
       })
       .addCase(fetchCart.rejected, (state, action) => {
         state.loading = false;
         state.error = action.error.message;
       });
   },
+ 
 });
 
 export { fetchCart };
-export const {calculateTotalAmount} = cartSlice.actions
+export const {calculateTotalAmount , increase , decrease} = cartSlice.actions
 export default cartSlice.reducer;
