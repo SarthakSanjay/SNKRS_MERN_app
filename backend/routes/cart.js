e39366c94a4d3c27f9dd91abe2ad8112a59b852a
Sarthak Sanjay <sarthaksanjaycoll21@gmail.com>
2023-10-14 23:51:51 +0530

    add update route

--- backend/routes/cart.js
+++ backend/routes/cart.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const router = express.Router()
-const {getCartItems , deleteAllCartItems , deleteCartItem , addToCart} = require('../controllers/cartController')
+const {getCartItems , deleteAllCartItems , deleteCartItem , addToCart , updateCartQuantity} = require('../controllers/cartController')
 router.route('/').get(getCartItems)
 router.route('/add').post(addToCart)
 router.route('/deleteAll').delete(deleteAllCartItems)
-router.route('/:id').delete(deleteCartItem)
+router.route('/:id').delete(deleteCartItem).patch(updateCartQuantity)
 
 module.exports = router
