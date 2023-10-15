ed7654c30068f9b0a47a304f1af69d23c0dfe521
Sarthak Sanjay <sarthaksanjaycoll21@gmail.com>
2023-10-15 23:47:28 +0530

    add getSpecificCartItem route

--- backend/routes/cart.js
+++ backend/routes/cart.js
@@ -1,9 +1,16 @@
-const express = require('express')
-const router = express.Router()
-const {getCartItems , deleteAllCartItems , deleteCartItem , addToCart , updateCartQuantity} = require('../controllers/cartController')
-router.route('/').get(getCartItems)
-router.route('/add').post(addToCart)
-router.route('/deleteAll').delete(deleteAllCartItems)
-router.route('/:id').delete(deleteCartItem).patch(updateCartQuantity)
+const express = require("express");
+const router = express.Router();
+const {
+  getCartItems,
+  deleteAllCartItems,
+  deleteCartItem,
+  addToCart,
+  updateCartQuantity,
+  getSpecificCartItem
+} = require("../controllers/cartController");
+router.route("/").get(getCartItems);
+router.route("/add").post(addToCart);
+router.route("/deleteAll").delete(deleteAllCartItems);
+router.route("/:id").delete(deleteCartItem).patch(updateCartQuantity).get(getSpecificCartItem);
 
-module.exports = router
+module.exports = router;
