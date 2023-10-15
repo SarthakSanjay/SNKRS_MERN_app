549423ce837725f0e0b80b149e5dd7aa6589aa41
Sarthak Sanjay <sarthaksanjaycoll21@gmail.com>
2023-10-15 23:09:25 +0530

    modified cartSlice

--- frontend/src/store/cartSlice.js
+++ frontend/src/store/cartSlice.js
@@ -5,16 +5,14 @@ const initialState = {
   cart: [],
   loading: false,
   error: null,
-  total: 0,
+  totalItems: 0,
   totalAmount: 0,
-  quantity: 1
+  amount: 0
 };
-
-const url = 'http://localhost:3000/cart'
 
 const fetchCart = createAsyncThunk('cartSlice/fetchCart', async () => {
   try {
-    const response = await axios.get(url);
+    const response = await axios.get('http://localhost:3000/cart');
     // console.log(response.data.shoe)
     return response.data.shoe; // Return the 'shoe' data from the response
   } catch (error) {
@@ -22,35 +20,17 @@ const fetchCart = createAsyncThunk('cartSlice/fetchCart', async () => {
   }
 });
 
+const calculateTotalAmount = (items) =>{
+  return items.reduce((totalPrice, item)=>{
+    return totalPrice + item.shoeId.price * item.quantity
+  },0)
+}
+
 const cartSlice = createSlice({
   name: 'cart',
   initialState,
   reducers: {
-    increase: (state , {payload}) =>{
-      const cartItem = state.cart.find((item) => {
-        item._id === payload.id
-        console.log(item);
-      })
-
-      // cartItem.quantity += cartItem.quantity + 1
-      // console.log(cartItem.quantity)
-      // console.log(JSON.stringify(state.cart, null, 2));
-
-    },
-    decrease: (state , {payload}) =>{
-      const cartItem = state.cart.find((item) => item._id === payload.id)
-      cartItem.quantity += cartItem.quantity - 1
-    },
-    calculateTotalAmount : (state , action) =>{
-      let quantity = 0
-      let total = 0
-      state.cart.forEach((items) => {
-        quantity += items.quantity
-        total += quantity * items.shoeId.price
-      })
-      state.total = quantity
-      state.totalAmount = total
-    }
+  
   },
   extraReducers: (builder) => {
     builder
@@ -61,10 +41,8 @@ const cartSlice = createSlice({
         state.cart = action.payload;
         state.loading = false;
         state.error = null;
-        state.total = action.payload.length
-
-        state.quantity = action.payload[0].quantity
-        // console.log("payload",action.payload[0].quantity)
+        state.totalItems = action.payload.length
+        state.totalAmount = calculateTotalAmount(action.payload)
       })
       .addCase(fetchCart.rejected, (state, action) => {
         state.loading = false;
@@ -75,5 +53,5 @@ const cartSlice = createSlice({
 });
 
 export { fetchCart };
-export const {calculateTotalAmount , increase , decrease} = cartSlice.actions
+export const { increase , decrease} = cartSlice.actions
 export default cartSlice.reducer;
