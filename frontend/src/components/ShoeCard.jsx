c8a7a56132f744563b4d4d2790c51355d2323cbe
Sarthak Sanjay <sarthaksanjaycoll21@gmail.com>
2023-10-15 23:49:08 +0530

    add random img functionality

--- frontend/src/components/ShoeCard.jsx
+++ frontend/src/components/ShoeCard.jsx
@@ -10,7 +10,7 @@ const ShoeCard = ({ shoe, id }) => {
     <>
       <div className=" h-max w-52 m-10 cursor-pointer text-white">
         <Link to={`/shoe/${id}`}>
-          <img src={image[1]} className="w-full object-cover h-40" />
+          <img src={image[Math.floor(Math.random() * 3)]} className="w-full object-cover h-40" />
           <h3 className="ml-2 p-1">{productName}</h3>
           <h3 className="ml-2 p-1">Price: ${price}</h3>
         </Link>
