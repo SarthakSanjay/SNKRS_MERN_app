c499bcb57363189fe789ca1c503f1317701209ba
Sarthak Sanjay <sarthaksanjaycoll21@gmail.com>
2023-10-15 23:59:55 +0530

    some changes

--- frontend/src/components/cart/CartItems.jsx
+++ frontend/src/components/cart/CartItems.jsx
@@ -3,77 +3,86 @@ import { AiOutlineArrowUp, AiOutlineArrowDown } from "react-icons/ai";
 import axios from "axios";
 import { fetchCart } from "../../store/cartSlice";
 import { useDispatch, useSelector } from "react-redux";
-import {
-  calculateTotalAmount,
-  increase,
-  decrease,
-} from "../../store/cartSlice";
+// import { increase, decrease } from "../../store/cartSlice";
 
 const CartItems = ({ cartItem, id }) => {
+  const [quantity, setQuantity] = useState(1);
   const dispatch = useDispatch();
-  const {quantity,cart} = useSelector(state => state.cart)
-//   console.log('cart',cart);
-  // const {totalAmount} = useSelector(state => state.cart)
-  // const [quantity , setQuantity] = useState(1)
-  // const [amount , setAmount] = useState(cartItem.price)
-  useEffect(() => {
-    // setAmount(cartItem.price * quantity);
+  const {amount} = useSelector(store => store.cart)
 
-    dispatch(calculateTotalAmount(quantity));
-    // console.log(totalAmount);
-  }, [quantity, cartItem.price]);
-
-  // console.log('cartitems', cartItem)
-  const delShoeFromCart = () => {
-    axios
-      .delete(`http://localhost:3000/cart/${id}`)
-      .then(console.log("item deleted"))
-      .then(dispatch(fetchCart()));
-    alert("item deleted");
+  const fetchQuantity = () => {
+    axios.get(`http://localhost:3000/cart/${id}`).then((res) => {
+      setQuantity(res.data.shoe.quantity);
+    });
   };
-  // const increment = () =>{
-  //     setQuantity(quantity + 1)
 
-  // }
-  // const decrement = () =>{
-  //     if(quantity === 1) return
-  //     setQuantity(quantity - 1)
-  // }
+  const updateQuantity = (newQuantity) => {
+    axios.patch(`http://localhost:3000/cart/${id}`, { quantity: newQuantity });
+  };
+
+  const handleIncrease = () => {
+    setQuantity((prevQuantity) => prevQuantity + 1);
+    updateQuantity(quantity + 1);
+    // dispatch(fetchCart())
+  };
+  
+  const handleDecrease = () => {
+    if (quantity > 0) {
+      setQuantity((prevQuantity) => prevQuantity - 1);
+      updateQuantity(quantity - 1);
+      // dispatch(fetchCart())
+    }
+  };
+
+  const handleDelete = () => {
+    axios.delete(`http://localhost:3000/cart/${id}`).then(() => {
+      console.log("Item deleted");
+    });
+    dispatch(fetchCart())
+  };
+
+  if(quantity === 0){
+    handleDelete()
+    dispatch(fetchCart())
+  }
+  
+  useEffect(() => {
+    fetchQuantity();
+  }, [id ]); // Only fetch quantity when id changes
+
+  const { productName, image ,price} = cartItem;
+
   return (
-    <div className="bg-gray-800 h-[100px] w-[500px] flex justify-between items-center m-5 ">
-      <img src={cartItem.image[0]} className="h-full w-1/5 object-center " />
-      <div className="flex flex-col justify-around h-full ">
-        <h2>{cartItem.productName}</h2>
+    <div className="bg-gray-800 h-[100px] w-[500px] flex justify-between items-center m-5">
+      <img src={image[0]} className="h-full w-1/5 object-center" />
+      <div className="flex flex-col justify-around h-full">
+        <h2>{productName}</h2>
         <div className="bg-red-400 w-[120px] rounded-[5px] flex justify-between p-2">
           <button
-            onClick={() => {
-                dispatch(increase(id))
-                dispatch(fetchCart())
-            }}
+            onClick={handleIncrease}
             className="bg-gray-500 border-[1px] border-black rounded-[2px]"
           >
             <AiOutlineArrowUp />
           </button>
           Qty : {quantity}
           <button
-            onClick={()=>{
-
-                dispatch(decrease(id))
-            }}
+            onClick={handleDecrease}
             className="bg-gray-500 border-[1px] border-black rounded-[2px]"
           >
             <AiOutlineArrowDown />
           </button>
         </div>
       </div>
-      {/* <h1>${amount}</h1> */}
-
+      <div>
+      <p className="text-[12px]">Price :${price }</p>
+      <h1 className="text-[12px]">Amount :${price * quantity}</h1>
       <button
-        onClick={delShoeFromCart}
-        className="bg-red-900 h-[40px] w-20  p-2 mr-2 rounded-[2px] hover:bg-red-500"
+        onClick={handleDelete}
+        className="bg-red-900 h-[40px] w-20 p-2 mr-2 rounded-[2px] hover:bg-red-500"
       >
         remove
       </button>
+      </div>
     </div>
   );
 };
