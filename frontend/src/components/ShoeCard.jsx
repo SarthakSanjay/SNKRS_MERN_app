53866e8b05aaa2a229e47e8520f0b59ef07c3bea
Sarthak Sanjay <sarthaksanjaycoll21@gmail.com>
2023-10-16 23:41:52 +0530

    add wishlisted prop

--- frontend/src/components/ShoeCard.jsx
+++ frontend/src/components/ShoeCard.jsx
@@ -5,17 +5,18 @@ const ShoeCard = ({ shoe, id }) => {
   if (!shoe) {
     return null; 
   }
-  const { productName, price, image } = shoe;
+  const { productName, price, image ,wishlisted} = shoe;
   return (
     <>
       <div className=" h-max w-52 m-10 cursor-pointer text-white">
         <Link to={`/shoe/${id}`}>
+        
           <img src={image[Math.floor(Math.random() * 3)]} className="w-full object-cover h-40" />
           <h3 className="ml-2 p-1">{productName}</h3>
           <h3 className="ml-2 p-1">Price: ${price}</h3>
         </Link>
         <div className=" relative left-40 bottom-12 ">
-          <WishlistBtn text={false} id={id} />
+          <WishlistBtn text={false} id={id} wishlisted={wishlisted} />
         </div>
       </div>
     </>
