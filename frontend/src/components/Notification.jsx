e0cb58c86cf5b91843be96c997e8913c9b96642c
Sarthak Sanjay <sarthaksanjaycoll21@gmail.com>
2023-10-16 23:41:26 +0530

    change style

--- frontend/src/components/Notification.jsx
+++ frontend/src/components/Notification.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Notification = ({noti,title}) => {
   return (
     
-     <div className={`border-[2px] h-20 w-48 p-2 border-green-500 text-green-500 flex justify-center items-center ${noti} `}>
+     <div className={`border-[2px] h-20 w-48 p-2 border-green-500 text-green-500 flex justify-center items-center ${noti} absolute z-10 bg-green-950 `}>
             {title}
      </div>
   )
