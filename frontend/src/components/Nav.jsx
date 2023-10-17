7a4a86cf72a5e25500676792a05f99684bcb0039
Sarthak Sanjay <sarthaksanjaycoll21@gmail.com>
2023-10-18 01:49:50 +0530

    add dropdown component for small devices

--- frontend/src/components/Nav.jsx
+++ frontend/src/components/Nav.jsx
@@ -3,10 +3,11 @@ import Search from "./Search";
 import WishListIcon from "./wishlist/WishListIcon";
 import CartIcon from "./cart/CartIcon";
 import NavLinks from "./navItems/NavLinks";
+import Dropdown from "./navItems/Dropdown";
 
 const Nav = () => {
   return (
-    <div className="flex justify-evenly bg-zinc-800 h-14 items-center w-screen sticky top-0 max-sm:justify-between p-2 ">
+    <div className="flex justify-evenly bg-zinc-800 h-14 items-center w-screen sticky top-0 max-sm:justify-around p-2 ">
       <Logo className="w-1/3 " />
       <div className="max-sm:hidden">
       <NavLinks />
@@ -17,6 +18,9 @@ const Nav = () => {
       </div>
         <WishListIcon />
         <CartIcon />
+        <div className="">
+          <Dropdown />
+        </div>
       </div>
     </div>
   );
