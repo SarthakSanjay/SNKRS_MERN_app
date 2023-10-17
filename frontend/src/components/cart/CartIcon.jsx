3a97653764c3376630a58dc19b2a25f7cef383f3
Sarthak Sanjay <sarthaksanjaycoll21@gmail.com>
2023-10-18 01:50:32 +0530

    style change

--- frontend/src/components/cart/CartIcon.jsx
+++ frontend/src/components/cart/CartIcon.jsx
@@ -11,7 +11,7 @@ const CartIcon = () => {
   },[dispatch])
   return (
     <Link to='/cart'>
-    <div className=" border-red-700 border-[2px] h-14 w-14 flex justify-center items-center ">
+    <div className=" h-14 w-14 flex justify-center items-center ">
         <LiaShoppingBagSolid className="text-3xl text-white absolute " />
         <h1 className="text-white relative bottom-3 left-3  rounded-full bg-green-500 w-5 h-5 flex justify-center items-center " >{totalItems}</h1>
     </div>
