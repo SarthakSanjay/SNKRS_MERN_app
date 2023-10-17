288b9e077046809164164d4a211dc305e37dc33f
Sarthak Sanjay <sarthaksanjaycoll21@gmail.com>
2023-10-17 23:54:45 +0530

    style change

--- frontend/src/components/cart/CartIcon.jsx
+++ frontend/src/components/cart/CartIcon.jsx
@@ -11,9 +11,10 @@ const CartIcon = () => {
   },[dispatch])
   return (
     <Link to='/cart'>
-        <LiaShoppingBagSolid className="text-3xl text-white" />
-        <h1 className="text-white absolute top-2 right-24 rounded-full bg-green-500 w-5 h-5 flex justify-center items-center " >{totalItems}</h1>
-        
+    <div className=" border-red-700 border-[2px] h-14 w-14 flex justify-center items-center ">
+        <LiaShoppingBagSolid className="text-3xl text-white absolute " />
+        <h1 className="text-white relative bottom-3 left-3  rounded-full bg-green-500 w-5 h-5 flex justify-center items-center " >{totalItems}</h1>
+    </div>
     </Link>
   )
 }
