8f742747e234f09d806fd53bd00a1079c600d7b3
Sarthak Sanjay <sarthaksanjaycoll21@gmail.com>
2023-10-17 23:56:04 +0530

    reset wishlist state

--- frontend/src/components/wishlist/Wishlist.jsx
+++ frontend/src/components/wishlist/Wishlist.jsx
@@ -1,49 +1,83 @@
-import React, { useEffect } from 'react';
-import ShoeCard from '../ShoeCard';
-import { useDispatch, useSelector } from 'react-redux';
-import { fetchWishlist } from '../../store/wishlistSlice';
-import { deleteAll } from '../../store/wishlistSlice';
-import axios from 'axios';
+import React, { useEffect } from "react";
+import ShoeCard from "../ShoeCard";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchWishlist } from "../../store/wishlistSlice";
+import { deleteAll } from "../../store/wishlistSlice";
+import axios from "axios";
+import Spinner from "../Spinner";
 
 const Wishlist = () => {
   const dispatch = useDispatch();
-  const { loading , shoe , error } = useSelector((state) => state.wishlist);
+  const { loading, shoe, error } = useSelector((state) => state.wishlist);
   // console.log('shoe', shoe)
   const deleteAll = () => {
-    axios.delete('http://localhost:3000/wishlist/deleteAll')
-    .then(console.log("deletedALl"))
-    .then(dispatch(deleteAll()))
-    .then(dispatch(fetchWishlist()))
-    .catch(e => console.log(e.message))
+    axios
+      .delete("http://localhost:3000/wishlist/deleteAll")
+      .then(console.log("deletedALl"))
+      .then(dispatch(fetchWishlist()))
+      .catch((e) => console.log(e.message));
+  };
+  const handleDelete = () => {
+    changeWishlisted();
+    deleteAll();
+    dispatch(fetchWishlist());
+  };
+  const changeWishlisted = async () => {
+    await axios
+      .get("http://localhost:3000/shoe/all")
+      .then((res) => {
+        console.log("res", res.data.shoes);
 
-    
-  }
- 
+        res.data.shoes.forEach((item) => {
+          axios
+            .patch(`http://localhost:3000/shoe/${item._id}`, {
+              wishlisted: false,
+            })
+            .then(console.log("updated successfully"));
+        });
+      })
+      .catch((e) => console.log(e.message));
+  };
+
   useEffect(() => {
     dispatch(fetchWishlist());
   }, [dispatch]);
 
   if (loading) {
-    return <div className='bg-black h-screen w-screen flex justify-center items-center'>
-      <h1 className='text-white text-[40px]'>Loading ...</h1>
-    </div>;
+    return <Spinner />
+    
   }
   if (error) {
-    return <div className='bg-black h-screen w-screen flex justify-center items-center'>
-      <h1 className='text-white text-[40px]'>Something went wrong!!!</h1>
-    </div>;
+    return (
+      <div className="bg-black h-screen w-screen flex justify-center items-center">
+        <h1 className="text-white text-[40px]">Something went wrong!!!</h1>
+      </div>
+    );
   }
- 
+
   return (
     <>
-      <div className='min-h-screen w-screen p-10 flex flex-wrap'>
-      <button onClick={deleteAll} className="bg-pink-700 text-white rounded-[4px] p-2 fixed right-10 ">Clear wishlist</button>
-        {shoe && shoe.map((wishlistItem) => {
-          {/* console.log(wishlistItem.shoeId.productName) */}
-          return <ShoeCard key={wishlistItem._id} shoe={wishlistItem.shoeId} id={wishlistItem._id} /> 
-        })
-        }
-        
+      <div className="min-h-screen w-screen p-10 flex flex-wrap">
+        <button
+          onClick={handleDelete}
+          className="bg-pink-700 text-white rounded-[4px] p-2 fixed right-10 "
+        >
+          Clear wishlist
+        </button>
+        {shoe &&
+          shoe.map((wishlistItem) => {
+            {
+              /* console.log(wishlistItem.shoeId.productName) */
+            }
+            return (
+              <ShoeCard
+                key={wishlistItem._id}
+                shoe={wishlistItem.shoeId}
+                id={wishlistItem._id}
+                inWishlist={true}
+              />
+            );
+          })}
       </div>
     </>
   );
