191df7130c8957393113b8dee11c68bfc985da87
Sarthak Sanjay <sarthaksanjaycoll21@gmail.com>
2023-10-19 22:11:20 +0530

    add login component

--- frontend/src/components/auth/Login.jsx
+++ frontend/src/components/auth/Login.jsx
@@ -1,6 +1,12 @@
 const Login = () => {
   return (
-    <div>Login</div>
+    <div className="h-52 w-32">
+        <label>Email/username</label>
+        <input type="email" />
+        <label>password</label>
+        <input type="password" />
+        <button type="submit">Login</button>
+    </div>
   )
 }
 
