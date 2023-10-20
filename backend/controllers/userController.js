91c8b69fef447849f1e80fd6d5fe3dc636a736a3
Sarthak Sanjay <sarthaksanjaycoll21@gmail.com>
2023-10-20 23:00:08 +0530

    add user model

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,2 +1,10 @@
 const USER = require('../models/user')
 
+const login = async (req, res ) =>{
+    const {email , password } = req.body
+    const user =  await USER.find({email: email})
+    if(!user){
+        return res.status(404).json({msg: 'user not found'})
+    }
+    const userPassword = await USER.find({password: password})
+}
