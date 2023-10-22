0a7819256cfe12e05309227d800745360446d299
Sarthak Sanjay <sarthaksanjaycoll21@gmail.com>
2023-10-22 23:27:12 +0530

    add register component logic

--- frontend/src/components/auth/Register.jsx
+++ frontend/src/components/auth/Register.jsx
@@ -1,16 +1,43 @@
-const Register = () => {
-  const handleClick =() =>{
+import React, { useState } from 'react';
+import axios from 'axios';
 
-  }
+const Register = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault(); // Prevent the default form submission
+    axios.post('http://localhost:3000/register', {
+      email: email,
+      password: password,
+    })
+    .then((response) => {
+      // Handle the response as needed
+    })
+    .catch((error) => {
+      // Handle errors
+    });
+  };
+
   return (
     <div className="h-52 w-32">
+      <form onSubmit={handleSubmit}>
         <label>Email/username</label>
-        <input type="email" />
-        <label>password</label>
-        <input type="password" />
-        <button onClick={handleClick} type="submit">Register</button>
+        <input type="email" value={email} onChange={handleEmailChange} />
+        <label>Password</label>
+        <input type="password" value={password} onChange={handlePasswordChange} />
+        <button type="submit">Register</button>
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default Register
+export default Register;
