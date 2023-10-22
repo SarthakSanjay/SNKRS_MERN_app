e432d6febca30a19aebbcd6447773a37911b23b1
Sarthak Sanjay <sarthaksanjaycoll21@gmail.com>
2023-10-22 23:24:37 +0530

    add register component to test

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,12 +4,14 @@ import Nav from './components/Nav'
 import Notification from './components/Notification'
 import Shoes from './components/Shoes'
 import {Outlet } from 'react-router-dom'
+import Login from './components/auth/Login'
 
 function App() {
   
   return (
     <div className='bg-zinc-900 w-screen min-h-[100vh]'>
 
+        <Login />
         <Nav />
         {/* <Notification /> */}
         <Outlet />
