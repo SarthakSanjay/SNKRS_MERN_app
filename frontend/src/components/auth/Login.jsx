bb1a1168e4214b6a0da5bb2bb697ad57506522f5
Sarthak Sanjay <sarthaksanjaycoll21@gmail.com>
2023-10-22 23:27:31 +0530

    add login component logic

--- frontend/src/components/auth/Login.jsx
+++ frontend/src/components/auth/Login.jsx
@@ -1,13 +1,43 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
 const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault(); // Prevent the default form submission
+    axios.post('http://localhost:3000/register', {
+      email: email,
+      password: password,
+    })
+    .then((response) => {
+      // Handle the response as needed
+    })
+    .catch((error) => {
+      // Handle errors
+    });
+  };
+
   return (
     <div className="h-52 w-32">
+      <form onSubmit={handleSubmit}>
         <label>Email/username</label>
-        <input type="email" />
-        <label>password</label>
-        <input type="password" />
+        <input type="email" value={email} onChange={handleEmailChange} />
+        <label>Password</label>
+        <input type="password" value={password} onChange={handlePasswordChange} />
         <button type="submit">Login</button>
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
