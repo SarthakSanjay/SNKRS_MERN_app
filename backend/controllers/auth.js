c8b561b2574062f365d08fa86242331690942737
Sarthak Sanjay <sarthaksanjaycoll21@gmail.com>
2023-10-23 22:53:12 +0530

    need to fix some bugs

---
+++ backend/controllers/auth.js
@@ -0,0 +1,18 @@
+const USER = require('../models/user')
+
+const createUser = async(req, res) =>{
+    const user = await USER.find({email: req.body.email})
+    if(!user){
+     const user =   await USER.create({
+            email: req.body.email,
+            password: req.body.password
+        })
+
+        return res.status(200).json({msg:'new user created',user:user})
+    }
+
+    res.status(200).json({msg:"user already exist"})
+    res.redirect('/login')
+}
+
+module.exports = {createUser}
--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,8 @@ const wishlistRouter = require('./routes/wishlist')
 const shoeRouter = require('./routes/shoe')
 const categoryRouter = require('./routes/category')
 const cartRouter = require('./routes/cart')
-
+const { createUser } = require('./controllers/auth')
+const USER = require('./models/user')
 app.use(cors())
 app.use(express.json())
 app.use('/wishlist', wishlistRouter)
@@ -16,8 +17,10 @@ app.use('/shoe',shoeRouter)
 app.use('/category', categoryRouter)
 app.use('/cart' , cartRouter)
 
-app.post('/register',(req,res)=>{
-    console.log(req.body)
+app.post('/register', createUser)
+app.get('/register', (req,res) => {
+    const user =  USER.find({})
+    res.status(200).json({user:user})
 })
 
 const start = async () => {
