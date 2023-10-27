a8a9ad33870804c5c1ad08e74a50134d7ba6e560
Sarthak Sanjay <sarthaksanjaycoll21@gmail.com>
2023-10-27 19:19:22 +0530

    add login component using mui template

--- frontend/src/components/auth/Login.jsx
+++ frontend/src/components/auth/Login.jsx
@@ -1,49 +1,118 @@
-import React, { useState } from 'react';
+import * as React from 'react';
 import axios from 'axios';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link  from '@mui/material/Link';
+import { Link as RRDLink} from 'react-router-dom';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 
-const Login = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+function Copyright(props) {
+  return (
+    <Typography variant="body2" color="text.secondary" align="center" {...props}>
+      {'Copyright © '}
+      <Link color="inherit" href="/">
+        SNKRS
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
 
-  const handleEmailChange = (e) => {
-    setEmail(e.target.value);
-  };
+// TODO remove, this demo shouldn't need to reset the theme.
 
-  const handlePasswordChange = (e) => {
-    setPassword(e.target.value);
-  };
+const defaultTheme = createTheme();
 
-  const handleSubmit = (e) => {
-    e.preventDefault(); // Prevent the default form submission
+export default function Login() {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
     axios.post('http://localhost:3000/login', {
-      email: email,
-      password: password,
+      email: data.get('email'),
+      password: data.get('password'),
     })
-    .then((response) => {
-      // Handle the response as needed
-    })
-    .catch((error) => {
-      // Handle errors
+    console.log({
+      email: data.get('email'),
+      password: data.get('password'),
     });
   };
 
   return (
-    <div className=" h-screen w-screen bg-black text-white flex justify-center items-center ">
-      <form onSubmit={handleSubmit} className="h-52 w-32">
-        <label>Email/username</label>
-        <input 
-        className="text-green-500" type="email" value={email} onChange={handleEmailChange} />
-        <label>Password</label>
-        <input
-        className="text-green-500"
-          type="password"
-          value={password}
-          onChange={handlePasswordChange}
-        />
-        <button type="submit">Login</button>
-      </form>
-    </div>
+    <ThemeProvider theme={defaultTheme} >
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign in
+          </Typography>
+          <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              autoComplete="email"
+              autoFocus
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+            />
+            <FormControlLabel
+              control={<Checkbox value="remember" color="primary" />}
+              label="Remember me"
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Sign In
+            </Button>
+            <Grid container>
+              <Grid item xs>
+              <RRDLink to="#">
+                  Forgot password?
+              </RRDLink>
+              </Grid>
+              <Grid item>
+                <RRDLink to="/register" variant="body2">
+                  {"Don't have an account? Sign Up"}
+                </RRDLink>
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
+        <Copyright sx={{ mt: 8, mb: 4 }} />
+      </Container>
+    </ThemeProvider>
   );
-};
-
-export default Login;
+}
