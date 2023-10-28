551453f13bfd06cc6912c05f9f60e48bfe4f311a
Sarthak Sanjay <sarthaksanjaycoll21@gmail.com>
2023-10-28 23:13:21 +0530

    code fix

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -1,13 +1,23 @@
-const USER = require('../models/user')
+const USER = require("../models/user");
 
-const login = async( req ,res) =>{
-    const {email , password } = req.body
-    const user = await USER.find({email: email , password: password})
-    if(!user){
-        return res.status(404).json({msg:"can't login"})
+const login = async (req, res) => {
+  const { email, password } = req.body;
+  const user = await USER.findOne({ email: email });
+  console.log(user);
+//   console.log("user password", user.password)
+  if (user) {
+    if (user.password === password) {
+      return res
+        .status(200)
+        .json({ msg: "logged in successfully", success: true });
+    } else {
+      return res
+        .status(404)
+        .json({ msg: "password didn't matched", success: false ,password:false });
     }
-    console.log(user)
-    res.status(200).json({msg:"logged in successfully"})
-}
+  }
 
-module.exports = login
+  res.status(404).json({ msg: "can't login user not found", success: false });
+};
+
+module.exports = login;
