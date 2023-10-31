e33500dbe430118c368896b41539ac23772337a1
Sarthak Sanjay <sarthaksanjaycoll21@gmail.com>
2023-10-31 23:55:01 +0530

    add profile toggle

--- frontend/src/components/Profile.jsx
+++ frontend/src/components/Profile.jsx
@@ -1,8 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Profile = () => {
+  const [display , setDisplay] = useState(false)
+  const handleClick = () =>{
+    setDisplay(prev => !prev)
+  }
   return (
-    <div className="h-[100px] w-[100px] rounded-full ">Profile</div>
+    <>
+    <div
+     className=" border-[1px] border-red-500  text-white cursor-pointer "
+     onClick={handleClick}
+     >Profile</div>
+
+     {display ?
+      <div className='h-[90vh] w-[300px] bg-pink-900 absolute z-100 top-[55px] right-0  '>
+      side menu
+     </div>
+      : ""}
+
+    </>
   )
 }
 
