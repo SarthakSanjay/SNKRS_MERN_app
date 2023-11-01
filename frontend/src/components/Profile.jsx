5c66796a6c9ed0b3ae43825a7b56d1877ce5e3e1
Sarthak Sanjay <sarthaksanjaycoll21@gmail.com>
2023-11-01 23:40:43 +0530

    add register and login btn

--- frontend/src/components/Profile.jsx
+++ frontend/src/components/Profile.jsx
@@ -1,25 +1,34 @@
-import React, { useState } from 'react'
-
+import React, { useState } from "react";
+import Link from "react-redux";
 const Profile = () => {
-  const [display , setDisplay] = useState(false)
-  const handleClick = () =>{
-    setDisplay(prev => !prev)
-  }
+  const [display, setDisplay] = useState(false);
+  const handleClick = () => {
+    setDisplay((prev) => !prev);
+  };
   return (
     <>
-    <div
-     className=" border-[1px] border-red-500  text-white cursor-pointer "
-     onClick={handleClick}
-     >Profile</div>
+      <div
+        className=" border-[1px] border-red-500  text-white cursor-pointer "
+        onClick={handleClick}
+      >
+        Profile
+      </div>
 
-     {display ?
-      <div className='h-[90vh] w-[300px] bg-pink-900 absolute z-100 top-[55px] right-0  '>
-      side menu
-     </div>
-      : ""}
-
+      {display ? (
+        <div className="h-[90vh] w-[300px] bg-pink-900 absolute z-100 top-[55px] right-0  ">
+          side menu
+          <button className="bg-black rounded-[2px] text-white">
+            <Link to='/login'>Login</Link>
+          </button>
+          <button className="bg-black rounded-[2px] text-white">
+            <Link to='/register'>Register</Link>
+          </button>
+        </div>
+      ) : (
+        ""
+      )}
     </>
-  )
-}
+  );
+};
 
-export default Profile
+export default Profile;
