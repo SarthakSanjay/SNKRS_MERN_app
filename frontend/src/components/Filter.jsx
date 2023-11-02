2295246c6109af6ca183ec0fd054e37ab33f871c
Sarthak Sanjay <sarthaksanjaycoll21@gmail.com>
2023-11-02 23:01:44 +0530

    add filter component

---
+++ frontend/src/components/Filter.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Filter = () => {
+  return (
+    <div>
+        <ul>
+            <li>Brand</li>
+            <li>Rating</li>
+            <li>Price:Low to high</li>
+            <li>Price:High to low</li>
+        </ul>
+    </div>
+  )
+}
+
+export default Filter
