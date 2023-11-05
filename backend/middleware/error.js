f1092989fcbb7cfb5ac5a655924b95f6034a8f92
Sarthak Sanjay <sarthaksanjaycoll21@gmail.com>
2023-11-06 00:42:18 +0530

    need to make error handler

--- backend/middleware/error.js
+++ backend/middleware/error.js
@@ -1,5 +1,12 @@
-class ErrorHandler extends Error{
-    
-}
+// class ErrorHandler extends Error{
+//     constructor(message, statusCode)
+//     super(message)
+//     this.statusCode = statusCode || 500
 
-module.exports = ErrorHandler
+// }
+
+// module.exports = ErrorHandler
+
+
+//need to make a error handler for res error
+//and need to create async error
