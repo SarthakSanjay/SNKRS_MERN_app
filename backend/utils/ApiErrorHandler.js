67679cdd4df16828503106552a2031af0d231ab0
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2023-12-18 03:01:45 +0530

    Refactor code

--- backend/utils/ApiErrorHandler.js
+++ backend/utils/ApiErrorHandler.js
@@ -1,6 +1,21 @@
 class ApiErrorHandler{
-    constructor(status , message){
+    constructor(
+        status,
+         message = 'Something went wrong',
+         errors = [],
+         stack = ""
+         ){
+            super(message)
         this.status = status
         this.message = message
+        this.data = null
+        this.success = false
+        this.errors =errors
+
+        if(stack){
+            this.stack =stack
+        }else{
+            Error.captureStackTrace(this , this.constructor)
+        }
     }
 }
