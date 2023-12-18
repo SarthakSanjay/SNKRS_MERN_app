01f9daf4f8dab41ba4d1a62e6a0525a74633ce3a
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2023-12-18 23:23:53 +0530

    add async handler

--- backend/utils/AsyncHandler.js
+++ backend/utils/AsyncHandler.js
@@ -1,11 +1,17 @@
-const asyncHandler = (fn) => async(err, req,res, next) =>{
-    try {
-        await fn(req,res,next)
-    } catch (error) {
-        res.status(err.code || 500).json({
-            sucess:false,
-            message:err.message
-        })
-    }
+// const asyncHandler = (fn) => async(err, req,res, next) =>{
+//     try {
+//         await fn(req,res,next)
+//     } catch (error) {
+//         res.status(err.code || 500).json({
+//             sucess:false,
+//             message:err.message
+//         })
+//     }
 
+// }
+
+const asyncHandler = (requestHandler) =>{
+    (req ,res , next) =>{
+        Promise.resolve(requestHandler(req,res,next)).catch(err => next(err))
+    }
 }
