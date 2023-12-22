b7542e4e26937bccc3e2ca994df0615eb0f4114a
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2023-12-22 23:11:27 +0530

    fix bug

--- frontend/src/components/ShoeCard.jsx
+++ frontend/src/components/ShoeCard.jsx
@@ -4,15 +4,14 @@ import axios from "axios";
 import { fetchWishlist } from "../store/wishlistSlice";
 import { useDispatch } from "react-redux";
 import {AiOutlineDelete} from 'react-icons/ai'
-const ShoeCard = ({ shoe, id, wishlisted, inWishlist , color }) => {
+const ShoeCard = ({ shoe, id, wishlisted, inWishlist  }) => {
   
-  let colors = color.split('/')
-  
-
   const dispatch = useDispatch()
+
   if (!shoe) {
     return null;
   }
+
   const deleteFromWishlist = () => {
     axios.delete(`http://localhost:3000/wishlist/remove/${id}`)
       .then(() => {
@@ -22,9 +21,8 @@ const ShoeCard = ({ shoe, id, wishlisted, inWishlist , color }) => {
       .catch(error => {
         console.error('Error removing from wishlist:', error);
       });
-  
-   
   }
+
   const updateWishlisted = () =>{
     axios.patch(`http://localhost:3000/shoe/${shoe._id}`, { wishlisted: false })
     .then(() => {
@@ -39,7 +37,10 @@ const ShoeCard = ({ shoe, id, wishlisted, inWishlist , color }) => {
     deleteFromWishlist()
     updateWishlisted()
   }
-  const { productName, price, image } = shoe;
+
+  const { productName, price, image , color } = shoe;
+  let colors = color && color.split('/')
+
   return (
     <>
       <div className=" h-max w-52 m-10 cursor-pointer text-white">
@@ -48,33 +49,27 @@ const ShoeCard = ({ shoe, id, wishlisted, inWishlist , color }) => {
             src={image[0]}
             className="w-full object-cover h-40"
           />
-          
-
           <h3 className="ml-2 p-1">{productName}</h3>
           <div className="flex">
           <h3 className="ml-2 p-1">Price: <span className="text-green-500">${price}</span></h3>
           <div className="h-12 w-12 flex">
           <div className={`w-6 h-6 border-[1px] border-white mr-2 rounded-full`} 
-          style={{backgroundColor: colors[0].toLowerCase()}}></div>
+          style={{backgroundColor: colors[0].toLowerCase()}}
+          ></div>
 
           <p className={`w-6 h-6 border-[1px] border-white rounded-full`} 
-          style={{backgroundColor:colors[1] ? colors[1].toLowerCase():'white'}}></p>
+          style={{backgroundColor:colors[1] ? colors[1].toLowerCase():'white'}}
+          ></p>
           
         </div>
           </div>
-       
-
-      
-        
         </Link>
+
         <div className=" relative left-40 bottom-12  ">
           {inWishlist ? (
             <button
               onClick={handleDelete}
-              className="bg-red-900 h-[40px] w-10 flex justify-center items-center  rounded-[2px] hover:bg-red-500
-              
-              "
-            >
+              className="bg-red-900 h-[40px] w-10 flex justify-center items-center  rounded-[2px] hover:bg-red-500">
               <AiOutlineDelete />
             </button>
           ) : (
