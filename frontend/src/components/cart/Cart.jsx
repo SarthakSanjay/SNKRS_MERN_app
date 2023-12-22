0cb87dcafbcc141b05ccfb88fc7b2b4014de714b
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2023-12-22 23:13:47 +0530

    fix bug

--- frontend/src/components/cart/Cart.jsx
+++ frontend/src/components/cart/Cart.jsx
@@ -6,23 +6,14 @@ import { useDispatch, useSelector } from "react-redux";
 import Notification from "../Notification";
 import Spinner from "../Spinner";
 const Cart = () => {
-
-  const [ noti , setNoti] = useState("hidden")
-  const [title , setTitle] = useState('')
-
-
+  const [totalAmount , setTotalAmount] = useState(0)
   const dispatch = useDispatch();
-  const { cart, loading, error ,totalAmount , quantity } = useSelector((state) => state.cart);
-  // console.log("cart",cart)
-if(cart.length === 0) {
-  setTimeout(()=>{
-    setNoti('hidden')
-  },9000)
-}
+  const { cart, loading, error  , quantity } = useSelector((state) => state.cart);
+ console.log('carting ', cart)
   const clearCart = () => {
     axios
       .delete("http://localhost:3000/cart/deleteAll")
-      .then(setNoti(''))
+      // .then(setNoti(''))
       .then(dispatch(fetchCart()))
       .catch((e) => console.log(e.message));
 
@@ -30,11 +21,15 @@ if(cart.length === 0) {
       dispatch(fetchCart())
   };
  
+  
 
   useEffect(() => {
-    dispatch(fetchCart());
-        console.log(totalAmount);
-  }, [dispatch , quantity]);
+    axios.get('http://localhost:3000/cart')
+      .then(res => setTotalAmount(res.data.totalAmount))
+      .catch(err => console.log(err.message))
+    // dispatch(fetchCart());
+        console.log('use effect rendered')
+  }, [ cart]);
 
 
   if (loading) {
@@ -54,7 +49,7 @@ if(cart.length === 0) {
   }
   return (
     <div className="bg-slate-900 min-h-screen text-white w-screen flex flex-col items-center p-10">
-    <Notification noti={noti} title={title} />
+    {/* <Notification noti={noti} title={title} /> */}
       <button
         onClick={clearCart}
         
@@ -72,7 +67,7 @@ if(cart.length === 0) {
         );
       })}
      
-      <h1>Total: {totalAmount} </h1>
+      <h1 className="absolute left-0 p-10 text-3xl">Total: ₹<span className="text-3xl text-green-400">{totalAmount}</span> </h1>
       {/* <button
         className="bg-green-500 text-white p-2 rounded-[2px] hover:bg-green-700 "
         // onClick={calculateTotal}
