ffafc63a92b640f7f73dcd48bec900b48d5118ec
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2023-12-27 23:10:09 +0530

    Refactor code

--- frontend/src/components/ShoeCard.jsx
+++ frontend/src/components/ShoeCard.jsx
@@ -4,47 +4,50 @@ import axios from "axios";
 import { fetchWishlist } from "../store/wishlistSlice";
 import { useDispatch } from "react-redux";
 import {AiOutlineDelete} from 'react-icons/ai'
-const ShoeCard = ({ shoe, id, wishlisted, inWishlist  }) => {
-  
-  const dispatch = useDispatch()
+const ShoeCard = ({ shoe,wid,
+  // , id, wishlisted,
+   inWishlist
+  }) => {
+    const {_id, productName, price, image , color ,wishlisted } = shoe;
+  // const dispatch = useDispatch()
 
   if (!shoe) {
     return null;
   }
 
-  const deleteFromWishlist = () => {
-    axios.delete(`http://localhost:3000/wishlist/remove/${id}`)
-      .then(() => {
-        alert('Removed from wishlist');
-        dispatch(fetchWishlist());
-      })
-      .catch(error => {
-        console.error('Error removing from wishlist:', error);
-      });
-  }
+  // const deleteFromWishlist = () => {
+  //   axios.delete(`http://localhost:3000/wishlist/remove/${_id}`)
+  //     .then(() => {
+  //       alert('Removed from wishlist');
+  //       dispatch(fetchWishlist());
+  //     })
+  //     .catch(error => {
+  //       console.error('Error removing from wishlist:', error);
+  //     });
+  // }
 
-  const updateWishlisted = () =>{
-    axios.patch(`http://localhost:3000/shoe/${shoe._id}`, { wishlisted: false })
-    .then(() => {
-      console.log('Updated successfully');
-    })
-    .catch(error => {
-      console.error('Error updating wishlisted status:', error);
-    });
-  }  
+  // const updateWishlisted = () =>{
+  //   axios.patch(`http://localhost:3000/shoe/${shoe._id}`, { wishlisted: !wishlisted })
+  //   .then(() => {
+  //     console.log('Updated successfully');
+  //     console.log(!wishlisted)
+  //   })
+  //   .catch(error => {
+  //     console.error('Error updating wishlisted status:', error);
+  //   });
+  // }  
+  // const handleDelete = () =>{
+  //   deleteFromWishlist()
+  //   updateWishlisted()
+  // }
 
-  const handleDelete = () =>{
-    deleteFromWishlist()
-    updateWishlisted()
-  }
-
-  const { productName, price, image , color } = shoe;
+ 
   let colors = color && color.split('/')
 
   return (
     <>
       <div className=" h-max w-52 m-10 cursor-pointer text-white">
-        <Link to={`/shoe/${id}`}>
+        <Link to={`/shoe/${_id}`}>
           <img
             src={image[0]}
             className="w-full object-cover h-40"
@@ -68,12 +71,13 @@ const ShoeCard = ({ shoe, id, wishlisted, inWishlist  }) => {
         <div className=" relative left-40 bottom-12  ">
           {inWishlist ? (
             <button
-              onClick={handleDelete}
+              // onClick={handleDelete}
               className="bg-red-900 h-[40px] w-10 flex justify-center items-center  rounded-[2px] hover:bg-red-500">
               <AiOutlineDelete />
             </button>
           ) : (
-            <WishlistBtn text={false} id={id} wishlisted={wishlisted} />
+            <WishlistBtn text={false} id={_id} 
+            wishlisted={wishlisted} />
           )}
         </div>
       </div>
