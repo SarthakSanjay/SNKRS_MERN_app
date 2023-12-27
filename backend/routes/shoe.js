9347060afcce5ebda0bac5870efd1838dbb5729b
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2023-12-28 00:26:04 +0530

    remove update route

--- backend/routes/shoe.js
+++ backend/routes/shoe.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const router = express.Router()
-const  { addShoe , getAllShoes , getSpecificShoe ,updateShoeClicked} = require('../controllers/shoeController')
+const  { addShoe , getAllShoes , getSpecificShoe } = require('../controllers/shoeController')
 
 router.route('/all').get(getAllShoes)
-router.route('/:id').get(getSpecificShoe).patch(updateShoeClicked)
+router.route('/:id').get(getSpecificShoe)
 router.route('/addShoe').post(addShoe)
 
 module.exports = router
