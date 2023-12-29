de8a69ce4462db539a5d146e4fd5840029bdf9dd
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2023-12-29 23:46:28 +0530

    add middlware to hash and method to compare password

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-
+const bcrypt = require('bcrypt')
 const userSchema = new mongoose.Schema({
     email:{
         type: String,
@@ -9,7 +9,26 @@ const userSchema = new mongoose.Schema({
     password:{
         type: String,
         required: true
-    }
+    },
+    cart:[{
+        type:mongoose.Schema.Types.ObjectId,
+        ref: 'CART'
+    }],
+    wishlist:[{
+        type:mongoose.Schema.Types.ObjectId,
+        ref: 'WISHLIST'
+    }]
 })
-
+// to encrypt the password
+//this is the pre middleware provided by the mongoose , it just do the task assigned to it before the actual task given to the schema
+userSchema.pre('save',async function(next){
+    if(!this.isModified("password")) return next()
+    this.password = bcrypt.hash(this.password , 10)
+    next()
+   
+})
+// custom method to check the password
+userSchema.methods.isPassword = async function(password){
+  return await bcrypt.compare(password  , this.password)
+}
 module.exports = new mongoose.model('USER' , userSchema)
