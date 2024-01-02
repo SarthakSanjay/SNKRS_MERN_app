b283d36edcd37cd7c24ae24c174b05f3bb3ab32f
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2024-01-02 14:02:00 +0530

    fix bug

--- backend/controllers/loginController.js
+++ backend/controllers/loginController.js
@@ -1,24 +1,75 @@
 const USER = require("../models/user");
+const { ApiErrorHandler } = require("../utils/ApiErrorHandler");
+const { ApiResponse } = require("../utils/ApiResponseHandler");
+const { asyncHandler } = require("../utils/AsyncHandler");
+const generateAccessAndRefreshToken = async(userId) =>{
+  try {
+    const user = await USER.findById(userId)
+    const accessToken = await user.generateAccessToken()
+    const refreshToken = await user.generateRefreshToken()
+    user.refreshToken = refreshToken
 
-const login = async (req, res) => {
+    await user.save({validateBeforeSave:false}) //dont validate just save cause password will also kickin
+    
+    return { accessToken , refreshToken}
+  } catch (error) {
+    // console.log(error.message);
+    throw new ApiErrorHandler(500 , 'Something went wrong while generating access and refresh token')
+  }
+}
+const login = asyncHandler(async (req, res) => {
   const { email, password } = req.body;
   const user = await USER.findOne({ email: email });
   console.log(user);
-//   console.log("user password", user.password)
-  if (user) {
-    if (user.password === password) {
-      
-      return res
-        .status(200)
-        .json({ msg: "logged in successfully", success: true });
-    } else {
-      return res
-        .status(404)
-        .json({ msg: "password didn't matched", success: false ,password:false });
-    }
+  if(!user){
+    throw new ApiErrorHandler(404 ,"email is required ")
   }
 
-  res.status(404).json({ msg: "can't login user not found", success: false });
-};
+  const isPasswordValid = await user.isPassword(password)
+  if(!isPasswordValid){
+    throw new ApiErrorHandler(401 , "password is incorrect ")
+  }
+  const {accessToken , refreshToken } = await generateAccessAndRefreshToken(user._id )
 
-module.exports = login;
+  const loggedInUser = await USER.findById(user._id).select("-password -refreshToken")
+  //to send cookise we need to send options
+  const options ={
+    httpOnly :true , //by doing this it is only modifiable form server not the frontend 
+    secure : true
+  }
+  return res
+  .status(200)
+  .cookie("accessToken" , accessToken , options)
+  .cookie("refreshToken", refreshToken , options )
+  .json(
+    new ApiResponse(200,{
+      user: loggedInUser, accessToken , refreshToken ,
+    },"User LoggedIn Successfully")
+  )
+});
+
+const logout = asyncHandler(async(req,res)=>{
+   await USER.findByIdAndUpdate(
+      req.user._id,
+      {
+        $set:{
+          refreshToken: undefined
+        }
+      },
+      {
+        new: true
+      }
+    )
+    const options ={
+      httpOnly :true , //by doing this it is only modifiable form server not the frontend 
+      secure : true
+    }
+    return res.status(200)
+    .clearCookie("accessToken" , options)
+    .clearCookie("refreshToken" , options)
+    .json(
+      new ApiResponse(200 , {}  ,"User Logged out")
+    )
+})
+
+module.exports = {login , logout};
