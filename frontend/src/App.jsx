496cd8f12d9310c991280b1e36a6a91b93099d89
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2024-01-05 23:30:50 +0530

    add dropdown component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import Nav from './components/navItems/Nav'
 import Notification from './components/Misc/Notification'
 import {Outlet } from 'react-router-dom'
+import Dropdown from './components/navItems/Dropdown'
 
 function App() {
   
@@ -10,6 +11,7 @@ function App() {
     <div className='bg-zinc-900 w-screen min-h-[100vh]'>
 
         <Nav />
+        <Dropdown />
         {/* <Notification /> */}
         <Outlet />
     </div>
