42541c86fbeb8301a6f8994155b012098a5c6bf6
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2024-01-06 23:40:51 +0530

    refactor code

--- backend/routes/wishlist.js
+++ backend/routes/wishlist.js
@@ -1,10 +1,10 @@
 const express = require('express')
 const router = express.Router()
-const {addWishlist , deleteWishlist , deleteAllWishlist , getWishlist} = require('../controllers/wishlistController')
+const {addWishlist , deleteWishlist , deleteAllWishlist , getWishlist, getAllWishlist} = require('../controllers/wishlistController')
 
 router.route('/').get(getWishlist)
 router.route('/remove/:id').post(deleteWishlist)
-router.route('/deleteALl').delete(deleteAllWishlist)
+router.route('/deleteAll').delete(deleteAllWishlist)
 router.route('/add/:id').post(addWishlist)
-
+router.route('/all').get(getAllWishlist)
 module.exports = router
