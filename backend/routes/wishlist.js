fe39afd3d1d8f65ac26009e25127a32614c89fbf
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2024-01-08 23:04:35 +0530

    Refactor wishlist routes

--- backend/routes/wishlist.js
+++ backend/routes/wishlist.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const router = express.Router()
 const {addWishlist , deleteWishlist , deleteAllWishlist , getWishlist, getAllWishlist} = require('../controllers/wishlistController')
+const { verifyJWT } = require('../middleware/auth.middleware')
 
-router.route('/').get(getWishlist)
-router.route('/remove/:id').post(deleteWishlist)
+router.route('/').get(
+    // verifyJWT,
+    getWishlist)
+router.route('/remove/:id').delete(deleteWishlist)
 router.route('/deleteAll').delete(deleteAllWishlist)
 router.route('/add/:id').post(addWishlist)
 router.route('/all').get(getAllWishlist)
