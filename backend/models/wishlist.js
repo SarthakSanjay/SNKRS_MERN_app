79e1be36396ddbd59636909b52484d6450c93302
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2024-01-08 23:04:04 +0530

    Refactor wishlist model

--- backend/models/wishlist.js
+++ backend/models/wishlist.js
@@ -1,27 +1,14 @@
 const mongoose = require('mongoose')
 
-// const wishlistSchema = new mongoose.Schema({
-//     shoeId : {
-//         type: mongoose.Schema.Types.ObjectId,
-//         ref: 'SHOES'
-
-//     }
-// })
 const wishlistSchema = new mongoose.Schema({
-    // Define properties for wishlist
-    // For example, you might have a name or items in the wishlist
-    // Define your fields here based on your application's requirements
     shoeId: [{
         type: mongoose.Schema.Types.ObjectId,
-        // You can reference a different model if needed, for example, 'Product'
         ref: 'SHOES'
     }],
-    // You might include other fields specific to your wishlist
 
-    // Link the wishlist to the user
     user: {
         type: mongoose.Schema.Types.ObjectId,
-        ref: 'USER' // Reference the User model
+        ref: 'USER' 
     }
 }, {
     timestamps: true
