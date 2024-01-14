544ce554f8018eabc925b94e38f343ee1ba0ffce
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2024-01-14 23:27:19 +0530

    refactor code

--- backend/routes/cart.js
+++ backend/routes/cart.js
@@ -3,14 +3,17 @@ const router = express.Router();
 const {
   getCartItems,
   deleteAllCartItems,
-  deleteCartItem,
   addToCart,
-  updateCartQuantity,
-  getSpecificCartItem
+  getAllCartItems,
+  cartItemQuantity,
+  decreaseCartItem,
+  deleteFromCart
 } = require("../controllers/cartController");
 router.route("/").get(getCartItems);
+router.route('/all').get(getAllCartItems)
 router.route("/add").post(addToCart);
 router.route("/deleteAll").delete(deleteAllCartItems);
-router.route("/:id").delete(deleteCartItem).patch(updateCartQuantity).get(getSpecificCartItem);
-
+router.route("/delete").delete(decreaseCartItem)
+router.route('/quantity').get(cartItemQuantity)
+router.route('/remove').delete(deleteFromCart)
 module.exports = router;
