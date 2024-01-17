9124f2e22997e13e323a1966c3e88d7f81d4cbef
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2024-01-18 03:26:27 +0530

    style change

--- frontend/src/components/shoes/ShoesDetails.jsx
+++ frontend/src/components/shoes/ShoesDetails.jsx
@@ -3,31 +3,46 @@ import { useParams } from "react-router-dom";
 import WishlistBtn from "../wishlist/WishlistBtn";
 import AddToCartBtn from "../cart/AddToCartBtn";
 import axios from "axios";
-import {  useSelector } from "react-redux";
+import {  useDispatch, useSelector } from "react-redux";
 import Spinner from "../Misc/Spinner";
 import { getCookie } from "../../utils/cookie";
 import CartIcon from "../cart/CartIcon";
+import Notification from "../Misc/Notification";
+import Review from "../reviews/review";
 
 const ShoesDetails = () => {
   const { id } = useParams();
   const [shoes , setShoes] = useState('')
+  // console.log(shoes)
   const {cart} = useSelector((state) => state.cart)
   const [inCart , setInCart] = useState(false)
   const { shoe, loading, error } = useSelector((state) => state.wishlist);
-
+  const [addedToCart , setAddedToCart] = useState(false)
+  const [addedToWishlist , setAddedToWishlist] = useState(false)
+  const [image , setImage] = useState('')
   const handleClick = (e) => {
     setImage(e.target.src);
+    console.log('clicked');
   };
+  // console.log(inCart);
  
   const addToCart = async() => {
    await axios.post(`http://localhost:3000/cart/add?shoeId=${id}&userId=${getCookie('userId')}`)
+   setAddedToCart(true)
+   setTimeout(()=>{
+    setAddedToCart(false)
+   },800)
   };
 
   useEffect(() => {
+    // console.log('shoedetails');
     axios.get(`http://localhost:3000/shoe/${id}`)
-    .then(res => setShoes(res.data.shoe))
-    setInCart(cart.some(shoe => shoe._id === id))
-  }, [ shoe , cart ]);
+    .then(res =>{ 
+      setShoes(res.data.shoe)
+      setInCart(cart.some(shoe => shoe._id === id))
+    }).catch((error)=> console.log(error))
+    // console.log(cart.some(shoe => shoe._id === id));
+  }, [ id , cart , shoe , inCart]);
 
 
 if (loading) {
@@ -44,8 +59,10 @@ if (error) {
   return (<>
     {shoes ? 
  
-    <div className="h-screen w-screen bg-slate-900 p-10 flex ">
-      <div className="h-3/4 w-1/2 flex p-4 justify-evenly">
+    <div className="h-screen  w-screen bg-slate-900 p-10 flex relative ">
+    {addedToCart ? <Notification text={"Item added to cart"}/> : ''}
+    {addedToWishlist ? <Notification text={"Item added to wishlist"} wishlist={true}/> : ''}
+      <div className="h-3/4 w-1/2 sticky top-10    border-purple-500 flex p-4 justify-evenly">
         <div className="h-full w-1/6 p-2 ">
           {shoes.image && shoes.image.length > 0 && (
             <>
@@ -67,10 +84,10 @@ if (error) {
             </>
           )}
         </div>
-        <img src={shoes.image ? shoes.image[0] : ''} className="h-[500px] w-[500px] bg-center object-contain" />
+        <img src={image ? image : shoes.image[0]} className="h-[500px] w-[500px] bg-center object-contain" />
       </div>
 
-      <div className=" w-1/2 p-4 text-white">
+      <div className="h-auto w-1/2 p-4 text-white    border-red-600">
         <h1 className="text-4xl">{shoes.productName}</h1>
         <br />
         <h2>{shoes.category}&apos;s Shoes</h2>
@@ -88,7 +105,7 @@ if (error) {
           
         </div>
         <br />
-        <WishlistBtn text={true}  id={id} wishlisted={shoes.wishlisted} />
+        <WishlistBtn text={true}  id={id}  />
         <br />
         {
           inCart ? 
@@ -98,7 +115,10 @@ if (error) {
         
         
         }
+        <br />
+        <Review shoeId={id} />
       </div>
+      
     </div>:
      <Spinner />
      }
