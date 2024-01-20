4e7497a80b8c230bb17be79be0a4cd9240f33ef5
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2024-01-20 23:42:45 +0530

    style change of Notification alert

--- frontend/src/components/Misc/Notification.jsx
+++ frontend/src/components/Misc/Notification.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 
-const Notification = ({noti,title}) => {
+const Notification = ({text , wishlist}) => {
+  if(wishlist){
+    return  <div className={`border-[2px] h-20 w-48 p-2 border-pink-500 text-pink-500 flex justify-center items-center  absolute left-1/2 transform -translate-x-1/2 z-10 bg-pink-950 `}>
+    {text}
+</div>
+  }
   return (
     
-     <div className={`border-[2px] h-20 w-48 p-2 border-green-500 text-green-500 flex justify-center items-center ${noti} absolute z-10 bg-green-950 `}>
-            {title}
+     <div className={`border-[2px] h-20 w-48 p-2 border-green-500 text-green-500 flex justify-center items-center  absolute left-1/2 transform -translate-x-1/2 z-10 bg-green-950 `}>
+            {text}
      </div>
   )
 }
